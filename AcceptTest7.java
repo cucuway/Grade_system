4a4160759f5d095b59849a6167bc49626da1c4b5
AndydeTsai <stuandychen@gmail.com>
2015-01-11 22:40:53 +0800

    Create AcceptTest7.java

---
+++ AcceptTest7.java
@@ -0,0 +1,39 @@
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AcceptTest7 {
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	//加上timeout設定可以防止無限迴圈
+	@Test(timeout=1000)
+	public void testMain() throws IOException {
+		/*TODO
+		 * 自訂測試案例
+		 * 
+		 * 
+		 * 
+		 */
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+}
