773dbb02271844d9cfc1522631b2eb9f44fc0eb8
AndydeTsai <stuandychen@gmail.com>
2015-01-11 20:41:14 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+
+
+public class Main extends Object{
+
+	public static void main(String[] args) throws FileNotFoundException {
+		try{
+			UI aUI=new UI();
+			
+		}catch(NoSuchIDExceptions e1){
+			System.out.println();
+		} catch (NoSuchCommandExceptions e) {
+			System.out.println();
+		}	
+
+	}
+
+}
