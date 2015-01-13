e51c24878929d14cc42772c52826585f50864763
AndydeTsai <stuandychen@gmail.com>
2015-01-13 23:12:11 +0800

    Update AcceptTest7.java

--- AcceptTest7.java
+++ AcceptTest7.java
@@ -23,12 +23,21 @@ public class AcceptTest7 {
 	//加上timeout設定可以防止無限迴圈
 	@Test(timeout=1000)
 	public void testMain() throws IOException {
-		/*TODO
-		 * 自訂測試案例
-		 * 
-		 * 
-		 * 
-		 */
+		//把stdout記錄起來
+		PrintStream stdout = System.out;
+		//設定從鍵盤依序輸入"955002056\n", "G\n", "R\n", "E\n", "Q\n"		
+		ByteArrayInputStream  inContent = new ByteArrayInputStream("955002056\nG\nR\nE\nQ\n". getBytes());
+		System.setIn(inContent);
+		//設定螢螢幕輸出
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		//執行程式
+		Main.main(null);
+		//把System.out還原，並把結果多印一分到stdout以利Debug
+		System.setOut(stdout);
+		System.out.println(outContent.toString());
+		//比對螢幕輸出	
+		assertThat(outContent.toString(), containsString("許文馨排名第14"));
 		
 	}
 	
