e7d93e36d3af4f33a1eeb9f302e545e78a2ca53a
cucuway <icefish0515@gmail.com>
2015-01-13 23:38:30 +0800

    Update GradeSystems.java
    W也可以運作了！

--- GradeSystems.java
+++ GradeSystems.java
@@ -52,11 +52,11 @@ public class GradeSystems {
 			String tmpID = alist[i][0];
 			if(ID.equals(tmpID)){		
 				System.out.println(alist[i][1]+"成績:");
-				System.out.println("lab1:        "+alist[i][2]);
-				System.out.println("lab2:        "+alist[i][3]);
-				System.out.println("lab3:        "+alist[i][4]);
+				System.out.println("lab1:       "+alist[i][2]);
+				System.out.println("lab2:       "+alist[i][3]);
+				System.out.println("lab3:       "+alist[i][4]);
 				System.out.println("mid-term:   "+alist[i][5]);
-				System.out.println("final exam:  "+alist[i][6]);
+				System.out.println("final exam: "+alist[i][6]);
 				System.out.println("total grade:"+alist[i][7]);
 			}
 		}
@@ -87,21 +87,16 @@ public class GradeSystems {
 	
 	public void updateWeights (){
 		 showOldWeights() ;
-		 double[] iweights=new double[5];
-		 iweights=getNewWeights();
-		 weights[0]=iweights[0];
-		 weights[1]=iweights[1];
-		 weights[2]=iweights[2];
-		 weights[3]=iweights[3];
-		 weights[4]=iweights[4];
-		 System.out.println(weights[0]+weights[1]+weights[2]+weights[3]+weights[4]);
+		 getNewWeights();
+//		 System.out.println(weights[0]+" "+weights[1]+" "+weights[2]+" "+weights[3]+" "+weights[4]);
 		 for(int i=0; i<index; i++){
-				alist[i][7]=String.valueOf(calculateTotalGrade(weights, alist[index][2], alist[index][3], alist[index][4], alist[index][5], alist[index][6]));
-				System.out.print(alist[i][7]+"  ");
+//			String oldScore = alist[i][7];
+			alist[i][7]=String.valueOf(calculateTotalGrade(weights, alist[i][2], alist[i][3], alist[i][4], alist[i][5], alist[i][6]));
+//			System.out.println("Old:" + oldScore + " -> New:" + alist[i][7]);
 		 }
 	}
 
-	private double[] getNewWeights() {
+	public void getNewWeights() {
 		double[] tmpweights=new double[5];
 		
 		boolean RIGHT=false;
@@ -126,16 +121,20 @@ public class GradeSystems {
 			System.out.println("以上正確嗎? Y (Yes) 或 N (No) ");
 			String cmd=sc.next();
 			if(cmd.equalsIgnoreCase("y")){
-				RIGHT=true;
+				weights[0]=tmpweights[0];
+				weights[1]=tmpweights[1];
+				weights[2]=tmpweights[2];
+				weights[3]=tmpweights[3];
+				weights[4]=tmpweights[4];
+				break;
 			}
 		}
-		return tmpweights;		
+		return ;		
 	}
 
 
 	private void showOldWeights() {
-		System.out.println("\t舊配分");
-		
+		System.out.println("\t舊配分");		
 		System.out.println("\t\tlab1\t\t"+(int)(weights[0]*100)+"%");
 		System.out.println("\t\tlab2\t\t"+(int)(weights[1]*100)+"%");
 		System.out.println("\t\tlab3\t\t"+(int)(weights[2]*100)+"%");
